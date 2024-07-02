c3c69664b424b613fa15430f255b8f9a76539224
Jack Arrington <mail@jackarrington.com>
2024-07-02 18:59:45 -0500

    Correct English mistakes in read-write-atoms section

--- app/quick-start/read-write-atoms/markdown.ts
+++ app/quick-start/read-write-atoms/markdown.ts
@@ -1,19 +1,19 @@
 export const markdown = `
 # Read Write atoms
 
-These atoms are the combination of both read-only and write-only atoms.
+These atoms are a combination of both read-only and write-only atoms.
 
 ~~~js
 const count = atom(1);
 export const readWriteAtom = atom((get) => get(count),
-(get, set) => {
+  (get, set) => {
     set(count, get(count) + 1);
   },
 );
 ~~~
 
 The first parameter is for reading and the second is for modifying the atom value.
-Since the \`readWriteAtom\` is capable to read and set the original atom value, so we can only export \`readWriteAtom\` atom and can hide the original atom in a smaller scope. In this way we have to deal with less number of atoms in our app.
+Since the \`readWriteAtom\` is able to read and set the original atom value, so we export just the \`readWriteAtom\` atom and can hide the original atom in a smaller scope. This way we have to deal with fewer atoms in our app.
 
-See the code how we use only \`handleMouseMoveAtom\` to read and update both the \`dotsArray\` in our app.
+See how we use just \`handleMouseMoveAtom\` to read and update both the \`dotsArray\` in our app.
 `
